56ff49ce69759d34fa68b48ed426358a1f0dcacb
Jean-Baptiste THEPOT <jbthepot@gmail.com>
2022-10-05 18:04:47 +0200

    Création d'un fichier qui affiche les gares d'europe

---
+++ affichage_gares.py
@@ -0,0 +1,30 @@
+#Train stations
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+gares = pd.read_csv("train_stations_europe.csv")
+
+#Gares de France
+
+def affichage_gares_france():
+    gares_france= gares[gares["country"]=='FR']
+
+    gares_france_latitude =gares_france["latitude"]
+    gares_france_longitude = gares_france["longitude"]
+
+    plt.scatter(gares_france_longitude,gares_france_latitude, color="red")
+    plt.show()
+    return()
+
+#Gares de UK
+
+
+def affichage_gares_europe():
+    
+    gares_latitude =gares["latitude"]
+    gares_longitude = gares["longitude"]
+
+    plt.scatter(gares_longitude,gares_latitude, color="green")
+    plt.show()
+    return()
