1aeca4daf8ce46600b876d28409aba93f35a9b1b
lilinghui <myloverllh@gmail.com>
2022-11-09 14:51:08 +0100

    Branche LL

--- get_journey_sncf.py
+++ get_journey_sncf.py
@@ -10,8 +10,61 @@ import requests
 from requests.adapters import HTTPAdapter
 import itertools  # list operators
 
+def read_cog(path):
+    """
+    Read code officiel géographique data, in the level of arrondissement.
+    """
+    return pd.read_csv(path)
+
+def get_cog(name_city, db_cog):
+    """
+    Get the corresponding code officiel géographique for a given city, in the level of arrondissement.
+    """
+    code = db_cog.loc[db_cog['NCC'] == name_city.upper()]['CHEFLIEU'].values
+    if len(code) != 1:
+        print("Error code")
+        return -1
+    else:
+        return code[0]
+
+'''TO DO check list of parameters'''
+def form_parameters(city_depart,
+                    city_arrive,
+                    date,
+                    db_cog,
+                    min_nb_journey=3):
+    """
+    Get the formalised paramters for API
+    """
+    dt = datetime.strptime(date, '%d/%m/%Y %H:%M:%S')
+    t_string = datetime.strftime(dt, '%Y%m%dT%H%M%S')
+    params = {'from': "admin:fr:" + get_cog(city_depart, db_cog),
+              'to': "admin:fr:" + get_cog(city_arrive, db_cog),
+              'datetime': str(t_string),
+              'min_nb_journeys': min_nb_journey
+        }
+    return params
+
+def execute_req(api_url, authorisation, parameters):
+    """
+    Execute request of data
+    """
+    r = requests.get(
+        api_url,
+        params=parameters,
+        auth=(authorisation, '')
+    )
+    if r.status_code != 200:  # see HTTP errors
+        print("HTTP ERROR")
+        return -1
+    else:
+        return json.loads(r.content.decode())['journeys']
+
 '''TODO : understand sections(potential error), corresponding code geograph'''
 def read_info_journey(journey):
+    """
+    Get information of all the journeys
+    """
     h_depart = datetime.strptime(journey['departure_date_time'], '%Y%m%dT%H%M%S')
     h_arrive =  datetime.strptime(journey['arrival_date_time'], '%Y%m%dT%H%M%S')
     duration = journey['durations']['total']
@@ -19,39 +72,28 @@ def read_info_journey(journey):
     type_train = journey['sections'][-2]['display_informations']['commercial_mode']
     direction = journey['sections'][-2]['display_informations']['direction']
     nb_transfers = journey['nb_transfers']
+
     return [h_depart, h_arrive, duration, no_train, type_train, direction, nb_transfers]
 
 if __name__ == '__main__':
+    path = './data/arrondissement_2022.csv'
+    db_cog = read_cog(path)
     api_url = 'https://api.sncf.com/v1/coverage/sncf/journeys?'
-    auth =  "42f6483b-fd9a-483a-89b8-4286395fd523"
+    auth = "42f6483b-fd9a-483a-89b8-4286395fd523"
     O = 'Paris'
     D = 'Lyon'
-    dt = datetime.strptime("12/11/2022", '%d/%m/%Y')
-    t_start = datetime.strftime(dt, '%Y%m%dT%H%M%S')
+    dt = "12/11/2022 09:00:00"
 
-    # Parameters
-    params_val = {'from': "admin:fr:75056",
-                  'to': "admin:fr:69123",
-                  'datetime': str(t_start),
-                  'min_nb_journeys': 3
-        }
+    params = form_parameters(O, D, dt, db_cog, 3)
+    r = execute_req(api_url, auth, params)
 
-    #API query
-    r = requests.get(
-        api_url,
-        params = params_val,
-        auth=(auth, '')
-    )
-    if r.status_code != 200:  # see HTTP errors
-        print("HTTP ERROR")
-    else:
-        results = json.loads(r.content.decode())['journeys']
+    if r != -1:
         col_name = ['h_depart', 'h_arrive', 'duration', 'no_train', 'type_train', 'direction', 'nb_transfers']
         info = []
-        for journey in results:
+        for journey in r:
             info.append(read_info_journey(journey))
         df = pd.DataFrame(info, columns=col_name)
-        print(df.columns)
+        print(df.head())
 
 
 
