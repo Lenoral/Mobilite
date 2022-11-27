eb6580c3d0fca80f20904677e3ec085ece4a0016
lilinghui <myloverllh@gmail.com>
2022-11-27 19:59:45 +0100

    Branche LL

--- get_journey_sncf.py
+++ get_journey_sncf.py
@@ -20,12 +20,16 @@ def get_cog(name_city, db_cog):
     """
     Get the corresponding code officiel géographique for a given city, in the level of arrondissement.
     """
-    code = db_cog.loc[db_cog['NCC'] == name_city.upper()]['CHEFLIEU'].values
-    if len(code) != 1:
+    if type(name_city) is float:
         print("Error code")
         return -1
     else:
-        return code[0]
+        code = db_cog.loc[db_cog['NCCENR'] == name_city]['CHEFLIEU'].values
+        if len(code) != 1:
+            print("Error code")
+            return -1
+        else:
+            return code[0]
 
 '''TO DO check list of parameters'''
 def form_parameters(city_depart,
@@ -34,7 +38,7 @@ def form_parameters(city_depart,
                     db_cog,
                     min_nb_journey=3):
     """
-    Get the formalised paramters for API
+    Get the formalised parameters for API
     """
     dt = datetime.strptime(date, '%d/%m/%Y %H:%M:%S')
     t_string = datetime.strftime(dt, '%Y%m%dT%H%M%S')
@@ -58,9 +62,14 @@ def execute_req(api_url, authorisation, parameters):
         print("HTTP ERROR")
         return -1
     else:
-        return json.loads(r.content.decode())['journeys']
+        result = json.loads(r.content.decode())
+        if 'journeys' in result.keys():
+            return result['journeys']
+        else:
+            print('No data')
+            return -1
 
-'''TODO : understand sections(potential error), corresponding code geograph'''
+'''TODO : understand sections(potential error)'''
 def read_info_journey(journey):
     """
     Get information of all the journeys
@@ -76,24 +85,42 @@ def read_info_journey(journey):
     return [h_depart, h_arrive, duration, no_train, type_train, direction, nb_transfers]
 
 if __name__ == '__main__':
+    auth = "42f6483b-fd9a-483a-89b8-4286395fd523"
+    api_url = 'https://api.sncf.com/v1/coverage/sncf/journeys?'
     path = './data/arrondissement_2022.csv'
     db_cog = read_cog(path)
-    api_url = 'https://api.sncf.com/v1/coverage/sncf/journeys?'
-    auth = "42f6483b-fd9a-483a-89b8-4286395fd523"
-    O = 'Paris'
-    D = 'Lyon'
-    dt = "12/11/2022 09:00:00"
 
-    params = form_parameters(O, D, dt, db_cog, 3)
-    r = execute_req(api_url, auth, params)
+    # get info of SNCF train stations
+    # https://ressources.data.sncf.com/api/records/1.0/search/?dataset=liste-des-gares&q=&rows=10000&facet=fret&facet=voyageurs&facet=code_ligne&facet=departemen
+    path = './data/referentiel_gares_voyageurs.csv'
+    df_station = pd.read_csv(path, sep=';')
+    # select only train station not RER etc. Not sure about the meaning of cols...
+    df_station = df_station.loc[df_station["Niveau de service"] >= 2]
 
-    if r != -1:
-        col_name = ['h_depart', 'h_arrive', 'duration', 'no_train', 'type_train', 'direction', 'nb_transfers']
-        info = []
-        for journey in r:
-            info.append(read_info_journey(journey))
-        df = pd.DataFrame(info, columns=col_name)
-        print(df.head())
+    o = 'Bordeaux'
+    d = list(df_station['Commune'].unique())
+    stations_foreign = []
+    dt = "28/11/2022 09:00:00"
+    results = {}
+    for d_city in d:
+        if get_cog(d_city, db_cog) == -1:
+            print(d_city)
+        else:
+            params = form_parameters(o, d_city, dt, db_cog, 3)
+            r = execute_req(api_url, auth, params)
+
+            if r != -1:
+                col_name = ['h_depart', 'h_arrive', 'duration', 'no_train', 'type_train', 'direction', 'nb_transfers']
+                info = []
+                for journey in r:
+                    info.append(read_info_journey(journey))
+                df = pd.DataFrame(info, columns=col_name)
+                df['waiting_time'] = df['h_depart'].apply(lambda x: (datetime.strptime(dt, '%d/%m/%Y %H:%M:%S') - x).seconds)
+                df['total_time'] = df['waiting_time'] + df['duration']
+                results[d_city] = df
+    print('')
+
+
 
 
 
